eee307a0fab3695ebb5be362da8dd184ad898911
Andhitia Rama <andhitia.r@gmail.com>
2018-07-08 10:45:17 +0700

    [8.0.1.0.0] hr_service_app

---
+++ hr_service_app/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Human Resource Service App",
+    "summary": "Manage Human Resource Service",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "menu.xml",
+        "views/hr_service_config_setting_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
