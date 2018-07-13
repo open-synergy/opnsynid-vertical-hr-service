6d49e08b805dfae1caaaa85089f1edf5044ecf86
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-13 18:52:25 +0700

    [8.0.1.0.0][hr_service_skill]

---
+++ hr_service_skill/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Human Resource Service - Skill Management",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "hr_service_app",
+        "hr_skill"
+    ],
+    "data": [
+        "views/hr_skill_view.xml"
+    ],
+    "installable": True
+}
