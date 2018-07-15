c1c9a5dc87765b3e53bab2e486e5ff53d0a39d75
Andhitia Rama <andhitia.r@gmail.com>
2018-07-15 20:53:13 +0700

    [8.0.1.0.0] hr_service_job

---
+++ hr_service_job/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Human Resource Service - Job",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "hr_service_app",
+        "partner_contact_job_position"
+    ],
+    "data": [
+        "views/res_partner_job_position_views.xml"
+    ],
+    "installable": True
+}
